b2cade2952699a8492be73ad1b096bb294819fa4
Richard Wang <richardwa@google.com>
2022-01-10 16:33:27 +0900

    ok can we enumerate fake data

--- gcp/appengine/frontend_handlers.py
+++ gcp/appengine/frontend_handlers.py
@@ -80,7 +80,27 @@ def index():
 @blueprint.route('/list3')
 def list3():
   """Main page."""
-  return render_template('list.html')
+  vulnerabilities = [
+    {
+      "id": "OSV-2021-586",
+      "summary": "Use-of-uninitialized-value in void intra_prediction_angular<unsigned short>",
+      "packages": ["OSS-Fuzz/kimageformats"],
+      "versions": ["v5.80.0", "v5.80.0-rc1", "v5.81.0", "v5.81.0-rc1", "v5.81.0-rc2", "v5.82.0-rc1"]
+    },
+    {
+      "id": "OSV-2021-451",
+      "summary": "Heap-buffer-overflow in void apply_sao_internal<unsigned short>",
+      "packages": ["OSS-Fuzz/kimageformats"],
+      "versions": ["v5.80.0", "v5.80.0-rc1", "v5.81.0", "v5.81.0-rc1", "v5.81.0-rc2", "v5.82.0-rc1"]
+    },
+    {
+      "id": "GHSA-xrr9-rh8p-433v",
+      "summary": "Request smuggling is possible when both chunked TE and content length specified",
+      "packages": ["Maven/io.ktor:ktor-server-cio", "Maven/io.ktor:ktor-client-cio"],
+      "versions": ["1.0.0", "1.0.0-rc", "1.0.1", "1.1.0", "1.1.1", "1.1.2", "1.1.3", "1.1.4"]
+    }
+  ]
+  return render_template('list.html', vulnerabilities = vulnerabilities)
 
 
 def bug_to_response(bug, detailed=True):
