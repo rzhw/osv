ec2a6b609ab6111be796b491992badec7ef7e859
Richard Wang <richardwa@google.com>
2022-01-18 15:27:57 +0900

    switch to mwc-icon and mwc-textfield
    we instantly get to delete a heap of css and resolve the issue of turbo messing up the textfield needing to be js init
    
    yes the table still has this problem unfortunately there is no "mwc-data-table"
    
    also mwc is larger. we go from main.css 103kb vendors.js 430kb to main.css 48.8kb vendors.js 817kb

--- gcp/appengine/frontend3/src/index.js
+++ gcp/appengine/frontend3/src/index.js
@@ -1,14 +1,12 @@
 import './styles.scss';
+import '@material/mwc-icon';
+import '@material/mwc-icon-button';
+import '@material/mwc-textfield';
 
 console.log("hello world!");
 
 import {MDCDataTable} from '@material/data-table';
 const dataTable = new MDCDataTable(document.querySelector('.mdc-data-table'));
 console.log('dataTable', dataTable);
-
-import {MDCTextField} from '@material/textfield';
-const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
-import {MDCTextFieldIcon} from '@material/textfield/icon';
-const icon = new MDCTextFieldIcon(document.querySelector('.mdc-text-field-icon'));
 
 import * as Turbo from '@hotwired/turbo';
