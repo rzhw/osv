9c2072e61f5ad1d174181a241a5fafc961ff42e3
Richard Wang <richardwa@google.com>
2022-01-18 15:29:45 +0900

    does turbo work as a regular module import?
    yes, yes it does

--- gcp/appengine/frontend3/src/index.js
+++ gcp/appengine/frontend3/src/index.js
@@ -2,11 +2,10 @@ import './styles.scss';
 import '@material/mwc-icon';
 import '@material/mwc-icon-button';
 import '@material/mwc-textfield';
+import '@hotwired/turbo';
 
 console.log("hello world!");
 
 import {MDCDataTable} from '@material/data-table';
 const dataTable = new MDCDataTable(document.querySelector('.mdc-data-table'));
 console.log('dataTable', dataTable);
-
-import * as Turbo from '@hotwired/turbo';
