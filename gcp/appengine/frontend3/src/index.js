07f416dd1c0989a88a105c8dbeec336ecb0bc371
Richard Wang <richardwa@google.com>
2022-01-18 15:45:10 +0900

    blah

--- gcp/appengine/frontend3/src/index.js
+++ gcp/appengine/frontend3/src/index.js
@@ -4,8 +4,6 @@ import '@material/mwc-icon-button';
 import '@material/mwc-textfield';
 import '@hotwired/turbo';
 
-console.log("hello world!");
-
 import {MDCDataTable} from '@material/data-table';
 const dataTable = new MDCDataTable(document.querySelector('.mdc-data-table'));
 console.log('dataTable', dataTable);
